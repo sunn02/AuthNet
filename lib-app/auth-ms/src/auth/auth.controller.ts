06a2cd02156a5caa8d9bc05daad3a9e223d1c970
sunn02 <sollopez0002@gmail.com>
2025-10-08 16:32:52 -0300

    signUp added

--- lib-app/auth-ms/src/auth/auth.controller.ts
+++ lib-app/auth-ms/src/auth/auth.controller.ts
@@ -2,6 +2,7 @@
 import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { RegisterDTO } from './dto/register.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,6 +10,24 @@ export class AuthController {
   @Post('login')
   @HttpCode(HttpStatus.OK)
   signIn(@Body() loginDto: LoginDto) {
-    return this.authService.signIn(loginDto.username, loginDto.password);
+    const user = {
+      email: loginDto.email,
+      password: loginDto.password
+    };
+    console.log('Intentando iniciar sesión para:', user);
+    return this.authService.signIn(user);
+  }
+
+  @Post('signUp')
+  @HttpCode(HttpStatus.OK)
+  signUp(@Body() registerDto: RegisterDTO) {
+    const newUser = {
+      email: registerDto.email,
+      password: registerDto.password,
+      username: registerDto.username,
+      phoneNo: registerDto.phoneNo
+    };
+    console.log('Registrando usuario:', newUser);
+    return this.authService.register(newUser);
   }
 }
